e236402ed1217a2212d8e1f3fdb18398dcfc9f7a
Ayşegül Çığrıkçı <aysegulcigrikci@hotmail.com>
2022-01-03 13:56:34 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Conrete.EntityFramework;
+using DataAccess.Conrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID
+    // Open Closed Principle : Gelişime Açık Değişime kapalı mevcut kodlar kalır yeni özellik eklenir.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+
+            }
+
+        }
+    }
+}
