8058cb5a83d2149c15216de29787cfd28106c4f2
Ayşegül Çığrıkçı <aysegulcigrikci@hotmail.com>
2022-01-24 18:30:34 +0200

    24.01.2022 productDetailDto

--- DataAccess/Conrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Conrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -63,5 +64,10 @@ namespace DataAccess.Conrete.InMemory
             productToUpdate.UnitPrice = product.UnitPrice;
             productToUpdate.UnitsInStock = product.UnitsInStock;
         }
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
