e20e790b5998a436510f675c876697b22d444274
Ayşegül Çığrıkçı <aysegulcigrikci@hotmail.com>
2022-02-01 17:38:57 +0200

    01.02.2022 web api

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Conrete.InMemory;
 using Entities.Concrete;
@@ -12,10 +13,22 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-
+  
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            //business codes
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("ürün ismi en az 2 karakter olmalı");
+            }
+            _productDal.Add(product);
+
+            return new SuccessResult("ürün eklendi");
         }
 
         public List<Product> GetAll()
@@ -29,6 +42,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
